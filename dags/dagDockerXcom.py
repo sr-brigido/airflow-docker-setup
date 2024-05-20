3a332a2dea5def29ec6b73c4c3ab091a6c891111
Gabriel Ronchi Brigido <gabrielbrigido1@gmail.com>
2024-05-20 01:07:13 -0300

    feat(dagDockerXcom): finalizando dag
    foi finalizada a DAG de demonstração do artigo

--- dags/dagDockerXcom.py
+++ dags/dagDockerXcom.py
@@ -28,12 +28,27 @@ def docker_testes_xcom():
         retrieve_output_path="/tmp/script.out",  # nosec B108
     )
 
+    task2 = DockerOperator(
+        api_version="auto",
+        docker_url="tcp://docker-socket-proxy:2375",
+        container_name="printa-data",
+        auto_remove=True,
+        mount_tmp_dir=False,
+        image="xcom-docker-operator:latest",
+        task_id="printa_data",
+        command="task2",
+        force_pull=False,
+        environment={
+            "X_COM": '{{ task_instance.xcom_pull(task_ids="gera_data") }}'
+        },  # noqa E501
+    )
+
     @task
     def fim():
         """Task Vazia para marcar fim."""
         pass
 
-    inicio() >> task1 >> fim()
+    inicio() >> task1 >> task2 >> fim()
 
 
 dag = docker_testes_xcom()
